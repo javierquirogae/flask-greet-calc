75b8ab33a2fa9bc7a28a7d55f06c985386ee9f35
Javier Quiroga <javier.quiroga.e@gmail.com>
2023-01-03 16:03:38 -0500

    finished

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,53 @@
 # Put your app in here.
+from flask import Flask, request
+
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+
+
+@app.route('/add')
+def adds():
+    """adds"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(add(a,b))
+
+@app.route('/sub')
+def subtracts():
+    """subtracts"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(sub(a,b))
+
+@app.route('/mult')
+def multiplies():
+    """multiplies"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(mult(a,b))
+
+@app.route('/div')
+def divides():
+    """divides"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(div(a,b))
+
+op = {
+        "add": add,
+        "sub": sub,
+        "mult": mult,
+        "div": div,
+        }
+
+@app.route("/math/<oper>")
+def do_math(oper):
+    """do all."""
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(op[oper](a,b))
+
+   
